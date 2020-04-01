e193e01509f3c79e146863e10ed0ebfe86bfd03d
lokesh183 <lokeshmgl183@gmail.com>
2020-04-01 13:48:45 +0530

    netflix-eureka-naming-server

--- currency-exchange-service/currency-exchange-service/target/classes/data.sql
+++
@@ -1,8 +0,0 @@
-insert into currency_exchange(id, currency_from, currency_to, conversion_multiple,port) values
-(1000, 'USD','INR', 65,0);
-
-insert into currency_exchange(id, currency_from, currency_to, conversion_multiple,port) values
-(1001, 'EUR', 'INR', 90,0);
-
-insert into currency_exchange(id, currency_from, currency_to, conversion_multiple,port) values
-(1002, 'AUD', 'INR', 50,0);
